207ab4793012163266917154503d8d36dac901e9
EndingJason <www.zhangyi508@gmail.com>
2015-07-15 17:52:03 +0800

    add

---
+++ app/controllers/recruit_controller.rb
@@ -0,0 +1,7 @@
+class RecruitController < ApplicationController
+  def index
+  end
+  def show
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
-
+  get 'recruit/index'
+  get 'recruit/show'
   resources :sms, only: [:create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
