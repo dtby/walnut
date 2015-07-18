60240567da19fd8643d20f8ba94d959121a5cbf7
czying <823698255@qq.com>
2015-07-18 15:31:30 -0400

    modify interviews/index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 resources :interviews
+
   root "home#index"
 resources :recruitments
   resources :sms, only: [:create]
+  resources :interviews
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -103,5 +103,4 @@ ActiveRecord::Schema.define(version: 20150715090030) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "interviews", "iv_categories"
-
 end
