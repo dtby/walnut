a416a7ee72c66c4df68e9ef2a98b5e7d23eb4f23
fanwei <fwshun@163.com>
2015-07-20 13:39:16 +0800

    production email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: "http://115.29.211.21:4200" }
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
