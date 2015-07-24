83c8ff9f3cac9921d1021be907fa86aad438ae32
EndingJason <www.zhangyi508@gmail.com>
2015-07-24 13:35:07 +0800

    change percent

--- app/assets/javascripts/training_platform.js
+++ app/assets/javascripts/training_platform.js
@@ -2,8 +2,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require jquery.turbolinks
-//= require_tree ./training_platform/.
 //= require_tree ./ace/.
+//= require_tree ./training_platform/.
 //= require bootstrap
 //= require application
 
