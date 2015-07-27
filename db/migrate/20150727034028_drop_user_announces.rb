9c42ebd0363c9fc1d7e999f85bca3f3916531e2c
xiaocuixt <cjw624923@126.com>
2015-07-27 13:55:09 +0800

    add create_table method

--- db/migrate/20150727034028_drop_user_announces.rb
+++ db/migrate/20150727034028_drop_user_announces.rb
@@ -1,5 +1,6 @@
 class DropUserAnnounces < ActiveRecord::Migration
   def change
+  	create_table :user_announces
   	drop_table :user_announces
   end
 end
--- db/migrate/20150727034038_drop_project_announces.rb
+++ db/migrate/20150727034038_drop_project_announces.rb
@@ -1,5 +1,6 @@
 class DropProjectAnnounces < ActiveRecord::Migration
   def change
+  	create_table :project_announces
   	drop_table :project_announces
   end
 end
