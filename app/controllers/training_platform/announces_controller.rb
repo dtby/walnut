56d5977f1378484280caac0a8d65fd5645a27ce1
OnlyCZY <853852998@qq.com>
2015-07-27 02:16:34 -0400

    finish create announce

--- app/controllers/training_platform/announces_controller.rb
+++ app/controllers/training_platform/announces_controller.rb
@@ -1,47 +1,50 @@
 class TrainingPlatform::AnnouncesController < TrainingPlatform::ApplicationController
-  before_action :set_announce, only: [:show, :edit, :destroy]
-  before_action :set_project
+	before_action :set_announce, only: [:show, :edit, :destroy]
+	before_action :set_project
 
-  def index
-  	@announces = Announce.all
-  end
+	def index
+		@announces = Announce.where(project_id: params[:project_id]).order(created_at: :DESC)
+	end
 
-  def new
-  	@announce = Announce.new
-  	respond_with @announce
-  end
+	def new
+		@announce = Announce.new
+		respond_with @announce
+	end
 
-  def show
-  	respond_with @announce
-  end
+	def create   
+		@announce = Announce.create(announce_params)
+		@announce.project_id = params[:project_id]
+		@announce.user_id = current_user.id   
+		@announces = Announce.where(project_id: params[:project_id])
+		if @announce.save     
+			flash.now[:notice] = "公告创建成功"
+		end   
+		respond_with @announces
+	end
 
-  def edit
-  	respond_with @announce
-  end
+	def show
+		respond_with @announce
+	end
 
-  def update
-  	if @announce.update(announce_params)
-  		flash[:notice] = "公告更新成功"
-  	end
-  	respond_with @announce
-  end
+	def edit
+		respond_with @announce
+	end
 
-  def destroy
-  	respond_with @announce.destroy
-  end
+	def update
+		if @announce.update(announce_params)
+			flash[:notice] = "公告更新成功"
+		end
+		respond_with @announce
+	end
 
-  def create
-  	@announce = Announce.create(announce_params)
-  	if @announce.save
-  		flash[:notice] = "公告创建成功"
-  	end
-  	respond_with @announce
-  end
+	def destroy
+		respond_with @announce.destroy
+	end
 
-  private
-  	def announce_params
-  		params.require(:announce).permit(:id, :title, :description, :user_id, :project_id)
-  	end
+	private
+	def announce_params
+		params.require(:announce).permit(:id, :title, :description, :user_id, :project_id)
+	end
 
 	def set_announce
 		@announce = Announce.where(id: params[:id]).first
