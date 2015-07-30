807f81ea358366dc7be811a0350d0b4a72949ed0
EndingJason <www.zhangyi508@gmail.com>
2015-07-30 16:08:47 +0800

    add reload

--- app/controllers/training_platform/comments_controller.rb
+++ app/controllers/training_platform/comments_controller.rb
@@ -1,5 +1,8 @@
 class TrainingPlatform::CommentsController < TrainingPlatform::ApplicationController
   def index
+    #@commentable = Task.where(id: params[:commentable_id]).first
+     @commentable = eval("#{params[:commentable_name]}.where(id: #{params[:commentable_id]}).first")
+     respond_with @commentable
   end
   def create
     @commentable = Comment.add_comment params[:commentable_name], params[:commentable_id], params[:body]
