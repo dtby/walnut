a662e8cbca3874ea5386a53c48be303f8271a86d
OnlyCZY <853852998@qq.com>
2015-08-27 16:44:49 +0800

    test form selet

--- app/models/apply.rb
+++ app/models/apply.rb
@@ -1,24 +1,28 @@
 class Apply < ActiveRecord::Base
-	validates :openid, :sex, :phone, :qq, :email, :address, :situation, :degree, :way, :train_name, presence: true
+	validates :openid, :name, :sex, :phone, :qq, :email, :address, :situation, :degree, :way, :train_name, presence: true
+	validates :name, format: { with: /[\u4e00-\u9fa5]/ , message: "请输入正确的姓名"}
+	validates :phone, format: { with: /\A(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}\z/, message: "请输入正确的手机号码" }
+	validates :qq, format: { with: /[1-9][0-9]{4,}/, message: "请输入正确的QQ号码" }
+	validates :email, format: { with: /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/, message: "请输入正确的的email" }
 
 	#性别
-	enum sex: { open: true, close: false }
-	SEX = { open: '男', close: '女' }
+	enum sex: { man: true, woman: false }
+	SEX = { man: '男', woman: '女' }
 	#现状
 	enum situation: {
-		'在职' => 'on_work',
-		'待业' => 'wait_work',
-		'学生' => 'student'
+		work: 's1',
+		diswork: 's2',
+		student: 's3'
 	}
-	SITUATION = ['在职', '待业', '学生']
+	SITUATION = { work: '在职', diswork: '待业', student: '学生' }
 	#教育状况
 	enum degree: {
-		'研究生' => 'graduate',
-		'本科' => 'undergraduate',
-		'高职' => 'vocational',
-		'其他' => 'other'
+		master: 'w1',
+		university: 'w2',
+		vocation: 'w3',
+		other: 'w4'
 	}
-	DEGREE = ['研究生', '本科', '高职', '其他']
+	DEGREE = { master: '研究生', university: '本科', vocation: '高职', other: '其他' }
 	#了解渠道
 	enum way: {
 		'老师推荐' => 'teacher',
@@ -31,6 +35,6 @@ class Apply < ActiveRecord::Base
 	enum train_name: {
 		'ios开发' => 'ios',
 		'安卓开发' => 'android',
-		'web全栈' => 'web'
+		'WEB全栈' => 'web'
 	}
 end
