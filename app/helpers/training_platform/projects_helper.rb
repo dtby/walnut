7f6f426d3d4f1a0746a37bb86b5d4141d045d8e5
fanwei <fwshun@163.com>
2015-09-07 11:12:02 +0800

    project ops

--- app/helpers/training_platform/projects_helper.rb
+++ app/helpers/training_platform/projects_helper.rb
@@ -11,6 +11,7 @@ module TrainingPlatform::ProjectsHelper
     end
   end
 
+  # index 项目描述显示
   def show_project_desc(project)
     if project.default?
       "（总任务：#{project.tasks.count} 已完成：#{project.tasks.completes.count}
@@ -18,5 +19,17 @@ module TrainingPlatform::ProjectsHelper
     else
       project.description
     end
+  end
+
+  # 项目操作
+  def project_ops(project)
+    ops = ""
+    if project.follow?
+      ops = link_to("javascript:void(0);"){image_tag("iconfont-copy.svg", width: "20px") }
+    elsif project.together?
+      ops = link_to("javascript:void(0);"){image_tag("iconfont-apply.svg", width: "20px") }
+      ops += link_to("javascript:void(0);"){image_tag("iconfont-copy.svg", width: "20px") }
+    end
+    ops
   end
 end
--- app/helpers/wechat/applies_helper.rb
+++ app/helpers/wechat/applies_helper.rb
@@ -1,5 +1,3 @@
 module Wechat::AppliesHelper
-	def title
-		"报名入口"
-	end
+	
 end
